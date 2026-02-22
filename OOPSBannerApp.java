509c38974dd19239b220111caa752d29a7da06cc
Rakshak65 <rakshakpathak.1307@gmail.com>
2026-02-22 19:02:45 +0530

    UC1: Print OOPS banner text

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPS Banner Application
+ *
+ * UC1: Print literal text "OOPS" to the console.
+ *
+ * @author Rakshak Pathak
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Application entry point.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
